cba19495c08465783c240529f87abc214d32a20d
Raoul <raoul.michel1@gmail.com>
2022-03-17 15:16:26 +0100

    cart now fetches all needed datas for items in the basket

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,72 +1,76 @@
-let cart = JSON.parse(localStorage.getItem("cart"));
-console.log(cart);
+let items = JSON.parse(localStorage.getItem("cart"));
+console.table(items);
 
-for (let value of cart) {
-    const item = document.getElementById("cart__items");
+for (let item of items) {
+    fetch(`http://localhost:3000/api/products/${item.model}`)
+        .then((resp) => resp.json())
+        .then((details) => {
+            console.log(details);
+            const productView = document.getElementById("cart__items");
 
-    let article = document.createElement("article");
-    article.classList.add("cart__item");
+            let article = document.createElement("article");
+            article.classList.add("cart__item");
+            article.setAttribute("data-id", `${item.model}`); // LOCAL STORAGE
+            article.setAttribute("data-color", `${item.color}`); // LOCALSTORAGE
+            productView.appendChild(article);
 
-    article.setAttribute("data-id", `${cart.model}`);
-    article.setAttribute("data-color", `${cart.color}`);
-    item.appendChild(article);
+            let divImage = document.createElement("div");
+            divImage.classList.add("cart__item__img");
+            article.appendChild(divImage);
 
-    let divimage = document.createElement("div");
-    divimage.classList.add("cart__item__img");
-    article.appendChild(divimage);
+            let image = document.createElement("img");
+            image.setAttribute("src", `${details.imageUrl}`); // API
+            image.setAttribute("alt", `${details.altTxt}`); // API
+            divImage.appendChild(image);
 
-    let image = document.createElement("img");
-    image.setAttribute("src", `${cart.imageUrl}`); // API
-    image.setAttribute("alt", `${cart.altTxt}`); // API
-    divimage.appendChild(image);
+            let divContent = document.createElement("div");
+            divContent.classList.add("cart__item__content");
+            article.appendChild(divContent);
 
-    let divcontent = document.createElement("div");
-    divcontent.classList.add("cart__item__content");
-    article.appendChild(divcontent);
+            let divDescription = document.createElement("div");
+            divDescription.classList.add("cart__item__content__description");
+            divContent.appendChild(divDescription);
 
-    let divdescription = document.createElement("div");
-    divdescription.classList.add("cart__item__content__description");
-    divcontent.appendChild(divdescription);
+            let productName = document.createElement("h2");
+            productName.innerText = details.name; //API
+            divDescription.appendChild(productName);
 
-    let productName = document.createElement("h2");
-    productName.innerText = cart.name; //API
-    divdescription.appendChild(productName);
+            let productColor = document.createElement("p");
+            productColor.innerText = item.color; // LOCALSTORAGE
+            divDescription.appendChild(productColor);
 
-    let productColor = document.createElement("p");
-    productColor.innerText = cart.color;
-    divdescription.appendChild(productColor);
+            let productPrice = document.createElement("p");
+            productPrice.innerText = `${details.price} €`; //API
+            divDescription.appendChild(productPrice);
 
-    let productPrice = document.createElement("p");
-    productPrice.innerText = cart.price; //API
-    divdescription.appendChild(productPrice);
+            let divSettings = document.createElement("div");
+            divSettings.classList.add("cart__item__content__settings");
+            divContent.appendChild(divSettings);
 
-    let divsettings = document.createElement("div");
-    divsettings.classList.add("cart__item__content__settings");
-    divcontent.appendChild(divsettings);
+            let divQty = document.createElement("div");
+            divQty.classList.add("cart__item__content__settings__quantity");
+            divSettings.appendChild(divQty);
 
-    let divQty = document.createElement("div");
-    divQty.classList.add("cart__item__content__settings__quantity");
-    divsettings.appendChild(divQty);
+            let quantity = document.createElement("p");
+            quantity.innerText = "Qté : ";
+            divQty.appendChild(quantity);
 
-    let quantity = document.createElement("p");
-    quantity.innerText = "Qté : ";
-    divQty.appendChild(quantity);
+            let qtySelect = document.createElement("input");
+            qtySelect.setAttribute("type", "number");
+            qtySelect.classList.add("itemQuantity");
+            qtySelect.setAttribute("name", "itemQuantity");
+            qtySelect.setAttribute("min", "1");
+            qtySelect.setAttribute("max", "100");
+            qtySelect.setAttribute("value", `${item.qty}`); //LOCALSTORAGE
+            quantity.appendChild(qtySelect);
 
-    let qtyselect = document.createElement("input");
-    qtyselect.setAttribute("type", "number");
-    qtyselect.classList.add("itemQuantity");
-    qtyselect.setAttribute("name", "itemQuantity");
-    qtyselect.setAttribute("min", "1");
-    qtyselect.setAttribute("max", "100");
-    qtyselect.setAttribute("value", "42"); //cart qty
-    quantity.appendChild(qtyselect);
+            let divDelete = document.createElement("div");
+            divDelete.classList.add("cart__item__content__settings__delete");
+            divSettings.appendChild(divDelete);
 
-    let divDelete = document.createElement("div");
-    divDelete.classList.add("cart__item__content__settings__delete");
-    divsettings.appendChild(divDelete);
-
-    let deleteButton = document.createElement("p");
-    deleteButton.classList.add("deleteItem");
-    deleteButton.innerText = "Supprimer";
-    divDelete.appendChild(deleteButton);
+            let deleteButton = document.createElement("p");
+            deleteButton.classList.add("deleteItem");
+            deleteButton.innerText = "Supprimer";
+            divDelete.appendChild(deleteButton);
+        });
 }
