7c3d97011f9a11f969a6b4edd00edef1e9f3a89e
Raoul <raoul.michel1@gmail.com>
2022-03-30 09:25:15 +0200

    Total qty & total price

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,3 +1,7 @@
+//VARIABLE FOR COUNTING TOTAL ITEMS IN CART
+let totalQuantity = 0;
+//VARIABLE FOR COUNTING TOTAL PRICE
+let totalPrice = 0;
 //GETS DATA FROM LOCALSTORAGE
 let items = JSON.parse(localStorage.getItem("cart"));
 console.table(items);
@@ -8,7 +12,7 @@ for (let item of items) {
         .then((resp) => resp.json())
         //STORES DATAS IN details VARIABLE
         .then((details) => {
-            console.log(details);
+            console.log(details._id);
             //SETS MAIN ELEMENT'S POSITION IN DOM
             const productView = document.getElementById("cart__items");
             // CREATES article ELEMENT, GETS CLASS AND ATTRIBUTES AND DEFINES AS productView CHILD
@@ -24,43 +28,39 @@ for (let item of items) {
             article.appendChild(divImage);
             //CREATES image, SETS src AND alt ATTRIBUTES AND DEFINES AS divImage CHILD
             let image = document.createElement("img");
-            // FETCHES FROM API
+            //FETCHES THESE DATAS FROM API (URL AND TEXT)
             image.setAttribute("src", `${details.imageUrl}`);
             image.setAttribute("alt", `${details.altTxt}`);
             divImage.appendChild(image);
-
+            //CREATES div, ADDS class NAME AND DEFINES AS divContent CHILD
             let divContent = document.createElement("div");
             divContent.classList.add("cart__item__content");
             article.appendChild(divContent);
-
+            //SAME AS PREVIOUS
             let divDescription = document.createElement("div");
             divDescription.classList.add("cart__item__content__description");
             divContent.appendChild(divDescription);
-
+            //CREATES h2 ELEMENT AND DEFINES AS divDescription CHILD
             let productName = document.createElement("h2");
             productName.innerText = details.name; //API
             divDescription.appendChild(productName);
-
+            //SAME AS PREVIOUS BUT p
             let productColor = document.createElement("p");
             productColor.innerText = item.color; // LOCALSTORAGE
             divDescription.appendChild(productColor);
-
-            let productPrice = document.createElement("p");
-            productPrice.innerText = `${details.price}€`; //API
-            divDescription.appendChild(productPrice);
-
+            //CREATES div, ADDS class NAME AND DEFINES AS divContent CHILD
             let divSettings = document.createElement("div");
             divSettings.classList.add("cart__item__content__settings");
             divContent.appendChild(divSettings);
-
+            //CREATES div, ADDS class NAME AND DEFINES AS divSettings CHILD
             let divQty = document.createElement("div");
             divQty.classList.add("cart__item__content__settings__quantity");
             divSettings.appendChild(divQty);
-
+            //CREATES p, SETS inner text NAME AND DEFINES AS divQty CHILD
             let quantity = document.createElement("p");
             quantity.innerText = "Qté : ";
             divQty.appendChild(quantity);
-
+            //CREATES input, SETS A BUNCH OF ATTRIBUTES AND DEFINES AS quantity CHILD
             let qtySelect = document.createElement("input");
             qtySelect.setAttribute("type", "number");
             qtySelect.classList.add("itemQuantity");
@@ -69,15 +69,25 @@ for (let item of items) {
             qtySelect.setAttribute("max", "100");
             qtySelect.setAttribute("value", `${item.qty}`); // FROM LOCALSTORAGE
             quantity.appendChild(qtySelect);
-
+            //CREATES p, ADDS FETCHED DATA AS innerText AND DEFINES AS divDescription CHILD
+            let productPrice = document.createElement("p");
+            productPrice.innerText = `${details.price}€`; //API
+            divDescription.appendChild(productPrice);
+            //CREATES div, ADDS class NAME AND DEFINES AS divSettings CHILD
             let divDelete = document.createElement("div");
             divDelete.classList.add("cart__item__content__settings__delete");
             divSettings.appendChild(divDelete);
-
+            //CREATES div, ADDS class NAME AND DEFINES AS divSettings CHILD
             let deleteButton = document.createElement("p");
             deleteButton.classList.add("deleteItem");
             deleteButton.innerText = "Supprimer";
             divDelete.appendChild(deleteButton);
+            //SUM OF ALL ARTICLES
+            totalQuantity += item.qty;
+            document.getElementById("totalQuantity").textContent = `${totalQuantity}`;
+            //TOTAL PRICE
+            totalPrice += item.qty * details.price;
+            document.getElementById("totalPrice").textContent = `${totalPrice}`;
         });
-}
 
+}
